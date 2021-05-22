b25b332a2355f8037b537d356bb8c5954e61f052
ussury <meshalkins@ya.ru>
2021-05-22 13:41:54 +0300

    refactoring resultExpression

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -2,25 +2,27 @@ import { getRandomInt, play } from '../index.js';
 
 const rule = 'What is the result of the expression?';
 
-// eslint-disable-next-line consistent-return
-const resultExpression = (one, two, simbol) => {
-  if (simbol === '+') return String(one + two);
-  if (simbol === '-') return String(one - two);
-  if (simbol === '*') return String(one * two);
+const resultExpression = (firstNumber, secondNumber, simbol) => {
+  const operations = {
+    '+': String(firstNumber + secondNumber),
+    '-': String(firstNumber - secondNumber),
+    '*': String(firstNumber * secondNumber),
+  };
+  return operations[simbol];
 };
 
 const getSimbol = () => {
   const arr = ['+', '-', '*'];
-  const simbol = (coll) => (coll[getRandomInt(arr.length)]);
+  const simbol = (coll) => coll[getRandomInt(arr.length)];
   return simbol(arr);
 };
 
 const addGameData = () => {
-  const one = getRandomInt(20);
-  const two = getRandomInt(10);
+  const firstNumber = getRandomInt(20);
+  const secondNumber = getRandomInt(10);
   const simbol = getSimbol();
-  console.log(`Question: ${one} ${simbol} ${two}`);
-  return resultExpression(one, two, simbol);
+  console.log(`Question: ${firstNumber} ${simbol} ${secondNumber}`);
+  return resultExpression(firstNumber, secondNumber, simbol);
 };
 
 export default () => play(rule, addGameData);
